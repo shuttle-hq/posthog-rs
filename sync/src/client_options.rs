1d2ded3dd5a74237d26f7b34b1ad5d288f083fef
Guga Gongadze <gugongadze@gmail.com>
2023-12-12 18:19:50 +0400

    feat: support for region

--- sync/src/client_options.rs
+++ sync/src/client_options.rs
@@ -2,9 +2,6 @@ use std::time::Duration;
 
 use crate::client::Client;
 
-const API_ENDPOINT: &str = "https://app.posthog.com";
-const TIMEOUT: Duration = Duration::from_millis(800); // This should be specified by the user
-
 pub struct ClientOptions {
     pub(crate) api_endpoint: String,
     pub(crate) api_key: String,
@@ -12,15 +9,15 @@ pub struct ClientOptions {
 }
 
 impl ClientOptions {
-    pub fn new(api_key: impl ToString) -> ClientOptions {
+    pub fn new(api_key: String, api_endpoint: String, timeout: Duration) -> ClientOptions {
         ClientOptions {
-            api_endpoint: API_ENDPOINT.to_string(),
-            api_key: api_key.to_string(),
-            timeout: TIMEOUT,
+            api_endpoint,
+            api_key,
+            timeout,
         }
     }
 
-    pub fn api_endpoint(&mut self, api_endpoint: impl ToString) -> &mut Self {
+    pub fn api_endpoint(&mut self, api_endpoint: String) -> &mut Self {
         self.api_endpoint = api_endpoint.to_string();
         self
     }
@@ -32,11 +29,5 @@ impl ClientOptions {
 
     pub fn build(self) -> Client {
         Client::new(self)
-    }
-}
-
-impl From<&str> for ClientOptions {
-    fn from(api_key: &str) -> Self {
-        ClientOptions::new(api_key)
     }
 }
